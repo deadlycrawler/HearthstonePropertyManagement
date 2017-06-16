86f2e8224425bfc3641c97da779399ea3af657a0
deadlycrawler <deadlycrawler2@gmail.com>
2017-06-16 00:21:09 +0000

    added 1 TODO related to last post, th look into a lat long converter

--- app/src/main/java/com/tools/radio/hearthstonepropertymanagement/rent.java
+++ app/src/main/java/com/tools/radio/hearthstonepropertymanagement/rent.java
@@ -27,6 +27,7 @@ public class rent extends AppCompatActivity {
         //TODO: change snapsot view "beds,baths etc." to a horizontal scrolling view. `maybe
         //TODO: combine the address together into a single linear layout to simplify code,
         //TODO: update constructor for Word.java to include a lat long
+        //TODO: examain ways to include a lat long converter from decimal degrees mins secs to degress decimal to put into google streetView intent
 
         words.add(new Word(1650, "308 E 18TH", "Portales, NM 88130", R.drawable.house_example_1, 3, 2, 1820));
         words.add(new Word(1295, "2108 s Ave G", "Portales, NM 88130", R.drawable.house_example_2, 3, 2, 0));
