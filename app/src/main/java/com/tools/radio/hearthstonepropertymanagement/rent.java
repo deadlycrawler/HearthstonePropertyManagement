b70e4c9d507e25db5ae6235eeb363865684ba60c
deadlycrawler <deadlycrawler2@gmail.com>
2017-06-27 19:52:29 +0000

    added added more content to rent page

--- app/src/main/java/com/tools/radio/hearthstonepropertymanagement/rent.java
+++ app/src/main/java/com/tools/radio/hearthstonepropertymanagement/rent.java
@@ -29,10 +29,20 @@ public class rent extends AppCompatActivity {
         //TODO: update constructor for Word.java to include a lat long
         //TODO: examain ways to include a lat long converter from decimal degrees mins secs to degress decimal to put into google streetView intent
         //TODO: create search menu template for the rent/buy pages, *inProgress*
+        //TODO: CREATE SEPERATE results page that narrows results basied of of paramaters entered in the rent search page, recomend posting the same words constructer but filter it based off results
+        //TODO: update word constuctor to include firepalce and garage
 
+        //WORD CONSTRUCTOR(price, address line 1, address line 2, main image resource location,beds,baths,sq feet)
 
-        words.add(new Word(1650, "308 E 18TH", "Portales, NM 88130", R.drawable.house_example_1, 3, 2, 1820));
+        words.add(new Word(1195, "204 W 1st St tower loft", "Portales, NM 88130", R.drawable.house_example_2, 2, 2, 1000));
+        words.add(new Word(1295, "1802 W University", "Portales, NM 88130", R.drawable.house_example_2, 3, 2, 0));
+        words.add(new Word(1295, "1514 s Ave A", "Portales, NM 88130", R.drawable.house_example_2, 3, 2, 1685));
         words.add(new Word(1295, "2108 s Ave G", "Portales, NM 88130", R.drawable.house_example_2, 3, 2, 0));
+        words.add(new Word(1395, "1918 S Main", "Portales, NM 88130", R.drawable.house_example_2, 3, 2, 0));
+        words.add(new Word(1495, "1101 Libra Drive", "Portales, NM 88130", R.drawable.house_example_2, 3, 2, 0));
+        words.add(new Word(1495, "1900 Libra Drive", "Portales, NM 88130", R.drawable.house_example_2, 0, 0, 0));
+        words.add(new Word(1500, "430 E University", "Portales, NM 88130", R.drawable.house_example_2, 3, 2, 1918));
+        words.add(new Word(1650, "308 E 18TH", "Portales, NM 88130", R.drawable.house_example_1, 3, 2, 1820));
 
 
         //takes word objects and desplays them in a list view and casts them to textviews
