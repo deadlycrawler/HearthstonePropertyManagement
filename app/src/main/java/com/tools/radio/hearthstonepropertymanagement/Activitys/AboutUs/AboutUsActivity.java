bd491977159250b8a19382bbb7dc219bdb2f9720
dill <deadlycrawler2@gmail.com>
2017-10-20 21:20:35 -0600

    decided to impliment a contact button in the top menu instead of on each page for convience

---
+++ app/src/main/java/com/tools/radio/hearthstonepropertymanagement/Activitys/AboutUs/AboutUsActivity.java
@@ -0,0 +1,15 @@
+package com.tools.radio.hearthstonepropertymanagement.Activitys.AboutUs;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.tools.radio.hearthstonepropertymanagement.R;
+
+public class AboutUsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about_us);
+    }
+}
